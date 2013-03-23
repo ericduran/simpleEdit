962688934bc80b394cd27de6d9fe6b9cd527605f
Eric J. Duran <eric.duran7@gmail.com>
2013-03-22 23:06:34 -0400

    Updating grunt file for submodule

--- GruntFile.js
+++ GruntFile.js
@@ -6,12 +6,13 @@ module.exports = function(grunt) {
   // Load grunt plugins.
   grunt.loadNpmTasks('grunt-bower-requirejs');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-update-submodules');
 
   // Project configuration.
   grunt.initConfig({
     bower: {
       target: {
-        rjsConfig: './src/simple/main.js'
+        rjsConfig: 'src/simple/main.js'
       }
     },
     jshint: {
@@ -24,5 +25,5 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', ['bower', 'jshint']);
+  grunt.registerTask('default', ['bower', 'update_submodules', 'jshint']);
 };
--- src/simple/main.js
+++ src/simple/main.js
@@ -3,16 +3,18 @@
  * Load dependant scripts and bootstap the app.
  */
 
-"use strict";
-
 require.config({
-  paths: {
-    ace: "../thirdparty/ace/lib/ace",
-    angular: "../components/angular/angular.min",
-    angularui: "../components/angular-ui/build/angular-ui.min",
-    jstree: "../components/jstree/dist/jstree.min",
-    keymaster: "../components/keymaster/keymaster.min",
-    simple: "../simple"
+  "paths": {
+    "ace": "../thirdparty/ace/lib/ace",
+    "angular": "../components/angular/angular",
+    "angularui": "../components/angular-ui/build/angular-ui.min",
+    "jstree": "../components/jstree",
+    "keymaster": "../components/keymaster",
+    "simple": "../simple",
+    "angular-ui": "../components/angular-ui",
+    "font-awesome": "../components/font-awesome",
+    "jquery": "../components/jquery/jquery",
+    "jquery-ui": "../components/jquery-ui/ui/jquery-ui"
   }
 });
 
